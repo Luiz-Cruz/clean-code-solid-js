7d436141e46994f7caee2eecc7c5e855137c89a6
Luiz Cruz <luiz.cruz@mercadolivre.com>
2024-12-02 18:06:05 -0300

    Remove unnecessary code

--- organized-version-v1/repositories/discountRepository.js
+++
@@ -1,5 +0,0 @@
-import { discountCodes } from "../database/inMemoryDatabase.js";
-
-export const getDiscount = (code) => {
-    return discountCodes.find((discount) => discount.code === code) || { discount: 0 };
-};
--- organized-version-v1/repositories/taxRepository.js
+++
@@ -1,9 +0,0 @@
-import { taxRates } from "../database/inMemoryDatabase.js";
-
-export const getTaxRate = (country, state, category) => {
-    if (taxRates[country]) {
-        const stateRates = taxRates[country][state] || taxRates[country].default;
-        return stateRates[category] || stateRates.default;
-    }
-    return taxRates.default;
-};
